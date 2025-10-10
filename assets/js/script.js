a80524dc538bc536e85553fefa50f93f46dec2de
Snehit <84265877+Snehitc@users.noreply.github.com>
2025-10-10 19:55:32 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,14 @@ const darkClass = "dark-theme";
 // Load saved theme
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add(darkClass);
-  toggle.textContent = "🌞";
+  toggle.textContent = "☀︎";
 }
 
 toggle.addEventListener("click", () => {
   body.classList.toggle(darkClass);
   const isDark = body.classList.contains(darkClass);
   localStorage.setItem("theme", isDark ? "dark" : "light");
-  toggle.textContent = isDark ? "🌞" : "🌙";
+  toggle.textContent = isDark ? "☀︎" : "⏾";
 });
 
 // Expandable Research Abstracts
